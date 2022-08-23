eb486ce0bda7ea41accf6adb029892f48f786ced
Chris Sharp <sharpchris@gmail.com>
2022-08-23 10:41:06 -0400

    update solution for class

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -10,8 +10,8 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         This is an open source version of the word guessing game we all know and
-        love. It was edited by Chris Sharp, Educational Technologist at the UFIT 
-        Center for Instructional Technology and Training (CITT) - {' '}
+        love. It was edited by the UFIT Center for Instructional Technology 
+        and Training (CITT) - {' '}
         <a
           href="https://github.com/sharpchris/react-one-wordle"
           className="underline font-bold"
